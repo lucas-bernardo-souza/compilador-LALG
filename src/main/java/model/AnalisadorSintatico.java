226dcabc020d0aa417d6f9ac8f17afe71a80cfcc
lucas-bernardo-souza <lucasbernardodesouza510@gmail.com>
2025-05-27 07:25:11 -0300

    Comentários

--- src/main/java/model/AnalisadorSintatico.java
+++ src/main/java/model/AnalisadorSintatico.java
@@ -47,6 +47,7 @@ public class AnalisadorSintatico {
         Token lookahead = tokens.get(ponteiro);
 
         while (!pilha.isEmpty()) {
+            // retorna o topo da pilha sem removê-lo
             String topo = pilha.peek();
 
             if (isTerminal(topo) || topo.equals("$")) {
